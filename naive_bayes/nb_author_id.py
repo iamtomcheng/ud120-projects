f1927704e9dce1e0bdc551b118469d5b670116de
Hok Chuen (Tom) Cheng <h.cheng@cern.ch>
2017-03-22 02:13:44 -0400

    Finished lecture one and two.

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python
 
-""" 
-    This is the code to accompany the Lesson 1 (Naive Bayes) mini-project. 
+"""
+    This is the code to accompany the Lesson 1 (Naive Bayes) mini-project.
 
     Use a Naive Bayes Classifier to identify emails by their authors
-    
+
     authors and labels:
     Sara has label 0
     Chris has label 1
 """
-    
+
 import sys
 from time import time
 sys.path.append("../tools/")
@@ -21,13 +21,70 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-
-
+print ''
+#print features_train[:3]
+#print labels_train[:3]
+#print features_test[:3]
+#print labels_test[:3]
+#print ''
 
 #########################################################
 ### your code goes here ###
 
+from sklearn.naive_bayes import GaussianNB
+
+print 'Create Gaussian Naive Bayes classifier.'
+clf = GaussianNB()
+
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+t1 = time()
+clf.predict(features_test)
+print "prediction time:", round(time()-t1, 3), "s"
+
+print "accuracy:", round(clf.score(features_test, labels_test), 3)
+print "mislabeled:", (clf.predict(features_test)!=labels_test).sum(), "/", len(labels_test)
+print ''
 
 #########################################################
 
+from sklearn.naive_bayes import MultinomialNB
+
+print 'Create Naive Bayes classifier for multinomial models.'
+clf = MultinomialNB()
+
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+t1 = time()
+clf.predict(features_test)
+print "prediction time:", round(time()-t1, 3), "s"
+
+print "accuracy:", round(clf.score(features_test, labels_test), 3)
+print "mislabeled:", (clf.predict(features_test)!=labels_test).sum(), "/", len(labels_test)
+print ''
+
+#########################################################
+
+from sklearn.naive_bayes import BernoulliNB
+
+print 'Create Naive Bayes classifier for multivariate Bernoulli models.'
+clf = BernoulliNB()
+
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+t1 = time()
+clf.predict(features_test)
+print "prediction time:", round(time()-t1, 3), "s"
+
+print "accuracy:", round(clf.score(features_test, labels_test), 3)
+print "mislabeled:", (clf.predict(features_test)!=labels_test).sum(), "/", len(labels_test)
+print ''
+
+#########################################################
 
--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python
 
-""" 
+"""
     This is the code to accompany the Lesson 2 (SVM) mini-project.
 
-    Use a SVM to identify emails from the Enron corpus by their authors:    
+    Use a SVM to identify emails from the Enron corpus by their authors:
     Sara has label 0
     Chris has label 1
 """
-    
+
 import sys
 from time import time
 sys.path.append("../tools/")
@@ -19,11 +19,112 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
+"""
+features_train = features_train[:len(features_train)/100]
+labels_train = labels_train[:len(labels_train)/100]
 
-
-
+print ''
+print 'Using 1% of training sample...'
+print ''
+#"""
 #########################################################
 ### your code goes here ###
+
+from sklearn.svm import SVC
+"""
+print 'Create SVM classfier with linear kernel'
+clf = SVC(kernel='linear')
+
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+t1 = time()
+clf.predict(features_test)
+print "prediction time:", round(time()-t1, 3), "s"
+
+print "accuracy:", round(clf.score(features_test, labels_test), 3)
+print "mislabeled:", (clf.predict(features_test)!=labels_test).sum(), "/", len(labels_test)
+print ''
+
+#########################################################
+
+print 'Create SVM classfier with rbf kernel for C=10.'
+#clf = SVC(kernel='rbf')
+clf = SVC(kernel='rbf',C=10.)
+
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+t1 = time()
+clf.predict(features_test)
+print "prediction time:", round(time()-t1, 3), "s"
+
+print "accuracy:", round(clf.score(features_test, labels_test), 3)
+print "mislabeled:", (clf.predict(features_test)!=labels_test).sum(), "/", len(labels_test)
+print ''
+
+#########################################################
+
+print 'Create SVM classfier with rbf kernel for C=100.'
+#clf = SVC(kernel='rbf')
+clf = SVC(kernel='rbf',C=100.)
+
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+t1 = time()
+clf.predict(features_test)
+print "prediction time:", round(time()-t1, 3), "s"
+
+print "accuracy:", round(clf.score(features_test, labels_test), 3)
+print "mislabeled:", (clf.predict(features_test)!=labels_test).sum(), "/", len(labels_test)
+print ''
+
+#########################################################
+
+print 'Create SVM classfier with rbf kernel for C=1000.'
+#clf = SVC(kernel='rbf')
+clf = SVC(kernel='rbf',C=1000.)
+
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+t1 = time()
+clf.predict(features_test)
+print "prediction time:", round(time()-t1, 3), "s"
+
+print "accuracy:", round(clf.score(features_test, labels_test), 3)
+print "mislabeled:", (clf.predict(features_test)!=labels_test).sum(), "/", len(labels_test)
+print ''
+#"""
+#########################################################
+
+print 'Create SVM classfier with rbf kernel for C=10000.'
+#clf = SVC(kernel='rbf')
+clf = SVC(kernel='rbf',C=10000.)
+
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+t1 = time()
+pred = clf.predict(features_test)
+print "prediction time:", round(time()-t1, 3), "s"
+
+print "accuracy:", round(clf.score(features_test, labels_test), 3)
+print "mislabeled:", (clf.predict(features_test)!=labels_test).sum(), "/", len(labels_test)
+print ''
+
+print '10: ', pred[10]
+print '26: ', pred[26]
+print '50: ', pred[50]
+
+print 'Chris: ', list(pred).count(1), "/", len(labels_test)
+print 'Sara : ', list(pred).count(0), "/", len(labels_test)
 
 #########################################################
 
